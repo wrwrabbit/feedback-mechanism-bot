71c3267939f830c9f0a7f9638df64eadae363f20
barfu296 <102421756+barfu296@users.noreply.github.com>
2023-09-03 22:09:19 +0300

    feat: delete

--- src/main/kotlin/by/cp/feedback/mechanism/bot/behaviour/common/delete.kt
+++ src/main/kotlin/by/cp/feedback/mechanism/bot/behaviour/common/delete.kt
@@ -10,12 +10,17 @@ import dev.inmo.tgbotapi.extensions.behaviour_builder.BehaviourContext
 import dev.inmo.tgbotapi.extensions.utils.extensions.raw.from
 import dev.inmo.tgbotapi.types.message.abstracts.CommonMessage
 import dev.inmo.tgbotapi.types.message.content.TextContent
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
 import kotlin.random.Random
+
+val userDeletionsMap = mutableMapOf<Long, Int>()
 
 fun delete(): suspend BehaviourContext.(CommonMessage<TextContent>) -> Unit = tryF { message ->
     val userId: Long = message.from?.id?.chatId ?: throw FromNotFoundException()
     val count = UserDeletionRepository.countDayAgo(userId)
-    if (count <= Random.nextLong(3, 5)) {
+    if (count <= getOrRandomAndPut(userId)) {
         UserRepository.deleteDataByUserId(userId)
         UserDeletionRepository.save(userId)
         UserRepository.changeUserStatus(userId, UserStatus.MUTED)
@@ -23,4 +28,17 @@ fun delete(): suspend BehaviourContext.(CommonMessage<TextContent>) -> Unit = tr
     } else {
         reply(message, "Лимит удалений исчерпан")
     }
+}
+
+fun getOrRandomAndPut(userId: Long): Int = userDeletionsMap[userId]
+    ?: Random.nextInt(3, 5).let { randomized ->
+        userDeletionsMap[userId] = randomized
+        randomized
+    }
+
+@Component
+class UserDeletionsCleaner {
+
+    @Scheduled(fixedRate = 30, timeUnit = TimeUnit.MINUTES)
+    fun clean() = userDeletionsMap.clear()
 }
