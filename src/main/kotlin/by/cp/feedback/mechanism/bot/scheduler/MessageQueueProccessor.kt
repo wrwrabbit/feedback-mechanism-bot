ae227d368ae8d8742d4402336b3fb3696b5e68ff
barfu296 <102421756+barfu296@users.noreply.github.com>
2023-09-06 11:24:14 +0300

    fix: queue

--- src/main/kotlin/by/cp/feedback/mechanism/bot/scheduler/MessageQueueProccessor.kt
+++ src/main/kotlin/by/cp/feedback/mechanism/bot/scheduler/MessageQueueProccessor.kt
@@ -8,6 +8,7 @@ import by.cp.feedback.mechanism.bot.repository.PollRepository
 import dev.inmo.tgbotapi.requests.send.SendTextMessage
 import dev.inmo.tgbotapi.types.toChatId
 import kotlinx.coroutines.runBlocking
+import mu.KotlinLogging
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.util.concurrent.TimeUnit
@@ -15,34 +16,42 @@ import java.util.concurrent.TimeUnit
 @Component
 class MessageQueueProccessor {
 
+    private val logger = KotlinLogging.logger {}
+
     @Scheduled(fixedRate = 3, timeUnit = TimeUnit.SECONDS)
     fun process() {
         runBlocking {
             MessageQueueRepository.select15().forEach { entry ->
-                when (entry.type) {
-                    REVIEW -> bot.execute(
-                        SendTextMessage(
-                            chatId = entry.userId.toChatId(),
-                            text = PollRepository.getById(entry.pollId)!!.toMessage(),
-                            replyMarkup = sendToUserReviewMarkup(entry.pollId)
-                        )
-                    )
-
-                    VOTE -> PollRepository.getById(entry.pollId)!!.let { poll ->
-                        bot.execute(
+                try{
+                    when (entry.type) {
+                        REVIEW -> bot.execute(
                             SendTextMessage(
                                 chatId = entry.userId.toChatId(),
-                                text = poll.toMessage(),
-                                replyMarkup = if (poll.allowMultipleAnswers) {
-                                    userVoteMultipleAnswersMarkup(poll.options, poll.id)
-                                } else {
-                                    userVoteSingleAnswerMarkup(poll.options, poll.id)
-                                }
+                                text = PollRepository.getById(entry.pollId)!!.toMessage(),
+                                replyMarkup = sendToUserReviewMarkup(entry.pollId)
                             )
                         )
+
+                        VOTE -> PollRepository.getById(entry.pollId)!!.let { poll ->
+                            bot.execute(
+                                SendTextMessage(
+                                    chatId = entry.userId.toChatId(),
+                                    text = poll.toMessage(),
+                                    replyMarkup = if (poll.allowMultipleAnswers) {
+                                        userVoteMultipleAnswersMarkup(poll.options, poll.id)
+                                    } else {
+                                        userVoteSingleAnswerMarkup(poll.options, poll.id)
+                                    }
+                                )
+                            )
+                        }
                     }
+                }catch (ex: Exception){
+                    logger.error(ex) { "Exception while sending message from queue" }
                 }
-                MessageQueueRepository.delete(entry)
+                finally {
+                    MessageQueueRepository.delete(entry)
+                }
             }
         }
     }
